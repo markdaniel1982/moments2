d2b2e6f4cd553bae8203947a021b5ce1edda5025
markdaniel1982 <>
2023-09-26 07:34:49 +0000

    create and configure interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://md82-drfapi-4f3d2bfe57a7.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
